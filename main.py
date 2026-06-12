342dc41355e44ca0ae749f3ae6bf063cda8e6c0a
Truong Thi Mai Uyen <truongthimaiuyen100805@gmail.com>
2026-06-12 22:08:06 +0700

    feat: tối ưu lại công thức tính gpa

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def tinh_diem_gpa_tuyen_tinh(diem_so):
+def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
-        return 4.0
-    elif diem_so < 0:
-        return 0.0
+        return 3.5
     else:
-        # Quy đổi tỷ lệ thẳng từ hệ 10 sang hệ 4 và lấy 2 chữ số thập phân
-        gpa = (diem_so / 10) * 4
-        return round(gpa, 2)
+        return (diem_so/10)*3.5
+    print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5)) 
